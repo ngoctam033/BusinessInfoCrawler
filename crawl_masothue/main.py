cf0024066047e2d24617ddfcf428118b650bffff
ngoctam033 <nguyenngoctam0332003@gmail.com>
2024-09-12 00:34:57 +0700

    .
    .

--- crawl_masothue/main.py
+++ crawl_masothue/main.py
@@ -26,49 +26,53 @@ for i in range(10):
     tax_list = tax_list_div.find_elements(By.CSS_SELECTOR, ':scope > div')
     # duyệt qua các thẻ div con
     for tax in tax_list:
-        # tạo một mảng để lưu thông tin mã số thuế
-        tax_infos = []
-        # lấy thuộc tính href của thẻ a
-        a = tax.find_element(By.TAG_NAME, 'a')
-        href = a.get_attribute('href')
-        # mở tab mới
-        driver.execute_script("window.open('');")
-        # chuyển qua tab mới
-        driver.switch_to.window(driver.window_handles[-1])
-        # mở link
-        driver.get(href)
-        # tìm thẻ table có class là 'table-taxinfo'
-        table = driver.find_element(By.CLASS_NAME, 'table-taxinfo')
-        # lấy nội dung text trong thẻ thead
-        thead = table.find_element(By.TAG_NAME, 'thead').text
-        # thêm thead vào mảng tax_infos
-        tax_infos.append(thead)
-        # lấy thẻ tbody
-        tbody = table.find_element(By.TAG_NAME, 'tbody')
-        # lấy các thẻ tr trong tbody
-        trs = tbody.find_elements(By.TAG_NAME, 'tr')
-        # duyêt qua các thẻ tr
-        for tr in trs:
-            # lấy các thẻ td trong tr
-            tds = tr.find_elements(By.TAG_NAME, 'td')
-            if tds[0].text == 'Mã số thuế':
-                # thêm thông tin mã số thuế vào mảng tax_infos
-                tax_infos.append(tds[1].text.strip())
-            if tds[0].text == 'Địa chỉ':
-                # thêm thông tin địa chỉ vào mảng tax_infos
-                tax_infos.append(tds[1].text.strip())
-            if tds[0].text == 'Điện thoại':
-                # thêm thông tin điện thoại vào mảng tax_infos
-                tax_infos.append(tds[1].text.strip())
-        print(tax_infos)
-        # mở file data.csv ở chế độ ghi thêm, uft-8
-        f = open('data.csv', 'a', encoding='utf-8')
         try:
-            # lần lượt ghi thông tin trong mảng tax_infos vào file data.csv
-            f.write(','.join(tax_infos) + '\n')
-        finally:
-            # đóng file lại
-            f.close()
+            # tạo một mảng để lưu thông tin mã số thuế
+            tax_infos = []
+            # lấy thuộc tính href của thẻ a
+            a = tax.find_element(By.TAG_NAME, 'a')
+            href = a.get_attribute('href')
+            # mở tab mới
+            driver.execute_script("window.open('');")
+            # chuyển qua tab mới
+            driver.switch_to.window(driver.window_handles[-1])
+            # mở link
+            driver.get(href)
+            # tìm thẻ table có class là 'table-taxinfo'
+            table = driver.find_element(By.CLASS_NAME, 'table-taxinfo')
+            # lấy nội dung text trong thẻ thead
+            thead = table.find_element(By.TAG_NAME, 'thead').text
+            # thêm thead vào mảng tax_infos
+            tax_infos.append(thead)
+            # lấy thẻ tbody
+            tbody = table.find_element(By.TAG_NAME, 'tbody')
+            # lấy các thẻ tr trong tbody
+            trs = tbody.find_elements(By.TAG_NAME, 'tr')
+            # duyêt qua các thẻ tr
+            for tr in trs:
+                # lấy các thẻ td trong tr
+                tds = tr.find_elements(By.TAG_NAME, 'td')
+                if tds[0].text == 'Mã số thuế':
+                    # thêm thông tin mã số thuế vào mảng tax_infos
+                    tax_infos.append(tds[1].text.strip())
+                if tds[0].text == 'Địa chỉ':
+                    # thêm thông tin địa chỉ vào mảng tax_infos
+                    tax_infos.append(f'"{tds[1].text.strip()}"')
+                if tds[0].text == 'Điện thoại':
+                    # thêm thông tin điện thoại vào mảng tax_infos
+                    tax_infos.append(tds[1].text.strip())
+            print(tax_infos)
+            # mở file data.csv ở chế độ ghi thêm, uft-8
+            f = open('data.csv', 'a', encoding='utf-8')
+            try:
+                # lần lượt ghi thông tin trong mảng tax_infos vào file data.csv
+                f.write(','.join(tax_infos) + '\n')
+            finally:
+                # đóng file lại
+                f.close()
+        except:
+            # chờ 60s
+            time.sleep(60)
         # đóng tab hiện tại
         driver.close()
         # quay lại tab chính
